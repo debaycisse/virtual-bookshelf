6f0a614940b8e60376c6fdaf2c9f2178614fb42e
Azeez Adebayo <debaycisse@gmail.com>
2024-10-05 20:16:32 +0100

    chore(endpoint): link the app to the routes
    Linked the server status endpoint to its controller

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+const port = process.env.EXP_PORT;
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}`);
+});
+
+module.exports = app;
