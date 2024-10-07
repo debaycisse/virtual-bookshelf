3e8328e2a224627536c782612c9f0833c945aa1f
Azeez Adebayo <debaycisse@gmail.com>
2024-10-07 06:50:04 +0100

    chore(signup endpoint): add signup endpoint
    Added the sign up endpoint controller to route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
 const AppController = require('../controllers/AppController');
+const UserAuthentication = require('../controllers/UserAuthentication');
 
 const routes = Router();
 
 routes.get('/api/v1/status', async (req, res) => {
   await AppController.getServerState(req, res);
+});
+
+routes.post('/api/v1/user/register', async (req, res) => {
+  await UserAuthentication.registerUser(req, res);
 });
 
 module.exports = routes;
