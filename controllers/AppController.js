d9e01ff99bd4b9e17d5bd57946dd051d9bb5a22e
Azeez Adebayo <debaycisse@gmail.com>
2024-10-07 06:45:27 +0100

    fixAppController): modify logic
    Correct AppController logic to remove redis and use jwt

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,11 @@
 const mongoDbClient = require("../utils/mongo");
-const redisClient = require('../utils/redis');
+// const redisClient = require('../utils/redis');
 const mime = require('mime-types');
 
 class BookShelfServer{
   static async getServerState(req, res) {
     const isAvailableMongo = await mongoDbClient.isAvailable();
-    const isAvailableRdis = await redisClient.isAvailable();
+    // const isAvailableRdis = await redisClient.isAvailable();
 
     if (!isAvailableMongo) {
       return res.status(500).json({
@@ -13,12 +13,12 @@ class BookShelfServer{
         detail: 'Mongo DB is not connected',
       });
     }
-    if (!isAvailableRdis) {
-      return res.status(500).json({
-        status: 'Server not available',
-        detail: 'Redis server is not connected',
-      });
-    }
+    // if (!isAvailableRdis) {
+    //   return res.status(500).json({
+    //     status: 'Server not available',
+    //     detail: 'Redis server is not connected',
+    //   });
+    // }
 
     res.setHeader('Content-Type', mime.contentType('json'));
     return res.status(200).json({
